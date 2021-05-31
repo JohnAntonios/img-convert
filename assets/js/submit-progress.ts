32081fa7c5be56da555bf024ba77d47bd90219b8
John Antonios <antonios.john@gmail.com>
2021-05-31 21:50:46 +1000

    TS for assets

--- assets/js/submit-progress.ts
+++ assets/js/submit-progress.ts
@@ -1,8 +1,8 @@
 const form = document.querySelector("form");
-const submitBtn = document.querySelector("form > button");
+const submitBtn = document.querySelector("form > button") as HTMLButtonElement;
 const spinner = document.querySelector("span#convert-spinner");
 
-const toggleSpinnerInButton = (showSpinner) => {
+const toggleSpinnerInButton = (showSpinner: boolean): void => {
   submitBtn.disabled = showSpinner;
   if (showSpinner) {
     spinner.classList.remove("d-none");
